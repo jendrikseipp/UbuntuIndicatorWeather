4e779175f378fff83cc040b9debbb63c58d9f654
Jendrik Seipp <jendrikseipp@web.de>
2016-07-09 17:34:06 +0200

    cleanup

--- indicator-weather.py
+++ indicator-weather.py
@@ -1,8 +1,5 @@
-
-#! /bin/sh
-""":"
-exec python $0 ${1+"$@"}
-"""
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # This file is part of indicator-weather.
 # Indicator Weather is free software; you can redistribute it and/or modify
@@ -17,23 +14,27 @@ exec python $0 ${1+"$@"}
 # Author(s):
 # (C) 2015 Kasra Madadipouya <kasra@madadipouya.com>
 
-import sys
-import gtk
-import appindicator
-import urllib
 import json
+import sys
+import urllib
 
-PING_FREQUENCY = 10 # minutes
+import appindicator
+import gtk
+
+
+PING_FREQUENCY_IN_MINUTES = 10
+
 
 class GetWeather:
     def __init__(self):
-        self.ind = appindicator.Indicator("weather-indicator",
-                                           "weather-clear",
-        #file:///usr/share/icons/ubuntu-mono-light/status/16 
-                                           appindicator.CATEGORY_APPLICATION_STATUS)
+        self.ind = appindicator.Indicator(
+            "weather-indicator",
+            "weather-clear",
+            appindicator.CATEGORY_APPLICATION_STATUS)
         self.ind.set_status(appindicator.STATUS_ACTIVE)
         self.menu_setup()
         self.ind.set_menu(self.menu)
+
     def menu_setup(self):
         self.menu = gtk.Menu()
         self.quit_item = gtk.MenuItem("Quit")
@@ -41,44 +42,37 @@ class GetWeather:
         self.quit_item.show()
         self.menu.append(self.quit_item)
         self.temp_item = gtk.MenuItem("")
-        #self.temp_item.connect("activate")
         self.temp_item.show()
         self.menu.append(self.temp_item)
+
     def main(self):
         self.get_weather()
-        gtk.timeout_add(PING_FREQUENCY * 60000, self.get_weather)
+        gtk.timeout_add(PING_FREQUENCY_IN_MINUTES * 60000, self.get_weather)
         gtk.main()
 
     def quit(self, widget):
         sys.exit(0)
 
     def get_weather(self):
-        lat,lon = self.get_location()
-        #3.0376280&lon=101.7060280
-        url = 'http://weatherwebservicecall.herokuapp.com/current?lat=' + lat + '&lon=' + lon
+        lat, lon = self.get_location()
+        url = (
+            'http://weatherwebservicecall.herokuapp.com/'
+            'current?lat={}&lon={}'.format(lat, lon))
         u = urllib.urlopen(url)
         data = u.read()
         j = json.loads(data)
-        #temp = j['main']['temp']
         temp = j['temperature']
-        city = j['geoLocation']
+        location = j['geoLocation']
         icon_name = j['iconName']
         icon_name = self.get_icon_name_local(icon_name)
-        if city :
-        	city = city.strip().split(",")
-        print temp
-        degree = u"\u2103"
-        if city :
-        	rtn_val = str(temp) + degree + ' ' + city[1]
-        else :
-        	rtn_val = str(temp) + degree + ' ' + city
-        self.ind.set_label(rtn_val)
+        city = location.strip().rsplit(",")[-1]
+        label = u'{temp}° {city}'.format(**locals())
+        self.ind.set_label(label)
         self.ind.set_icon(icon_name)
-        self.temp_item.get_child().set_text(rtn_val)        
+        self.temp_item.get_child().set_text(label)
         return True
 
-    def get_icon_name_local(self,icon_name):
-        print icon_name
+    def get_icon_name_local(self, icon_name):
         if icon_name == "01d":
             return "weather-clear"
         elif icon_name == "01n":
@@ -101,23 +95,17 @@ class GetWeather:
             return "weather-snow"
         elif icon_name in ("50d", "50n"):
             return "weather-fog"
-    
+
     def get_location(self):
-        
-        try:
-        	url = 'http://ipinfo.io/json/'
-        	u = urllib.urlopen(url)
-        	data = u.read()
-        	j = json.loads(data)
-        	loc = j['loc']
-        	y = loc.strip().split(",")
-        	lat = y[0]
-        	lon = y[1]
-        	return (lat,lon)
-        except:
-            return False,0
+        url = 'http://ipinfo.io/json/'
+        u = urllib.urlopen(url)
+        data = u.read()
+        info = json.loads(data)
+        loc = info['loc']
+        lat, lon = loc.strip().split(",")
+        return (lat, lon)
+
 
 if __name__ == "__main__":
     indicator = GetWeather()
     indicator.main()
-
